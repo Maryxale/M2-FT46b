1a4308bae4d8cd43ed5958f4c422ac8a9de2e11e
Maryxale <romerobencomo@gmail.com>
2024-01-11 02:04:58 -0300

    hw reactIntro lista

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,5 +1,26 @@
 import Card from './Card';
 
 export default function Cards(props) {
-   return <div></div>;
+   
+   const {characters} = props
+   return (
+   <div>
+      {
+         characters.map(char => (
+            <Card
+               key={char.id}
+               name={char.name}
+               status={char.status}
+               species={char.species}
+               gender={char.gender}
+               origin={char.origin.name}
+               image={char.image}
+               onClose={() => alert('Emulamos que se cierra la card')}
+               />
+               
+         ))
+      }
+      
+   </div>
+   )
 }
